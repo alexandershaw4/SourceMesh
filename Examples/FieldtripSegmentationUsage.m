0d0a5d86cfa580e9ff6be8326c2ffecb73ac51d3
Alex <alexandershaw4@gmail.com>
2018-09-14 15:35:17 +0100

    +++

--- Examples/FieldtripSegmentationUsage.m
+++ Examples/FieldtripSegmentationUsage.m
@@ -6,8 +6,8 @@ load coregseg_0001.mat      % saved fieldtrip segmentation
 segvol = segmentedmri.gray; % the gray matter volume
 
 
-% extract surface & write gifti file
-afigure,atemplate('mesh',segvol,'write','Alex')
+% extract surface, check orientation & write gifti file
+afigure,atemplate('mesh',segvol,'checkori','write','Alex')
 
 % Now load up the saved gifti object & plot curvature overlay heatmap on
 % leftside (for example)
