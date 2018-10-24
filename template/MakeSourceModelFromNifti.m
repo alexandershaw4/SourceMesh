980bf269a3b83c83cbbdc0378adcb212851d2002
Alex <alexandershaw4@gmail.com>
2018-10-24 18:52:15 +0100

    updated aal 90 template

---
+++ template/MakeSourceModelFromNifti.m
@@ -0,0 +1,36 @@
+
+
+mri = ft_read_mri('/Users/Alex/Dropbox/AAL_Template/AAL_Labels.nii');
+ana = mri.anatomy;
+
+S = size(ana);
+x = 1:S(1); 
+y = 1:S(2); 
+z = 1:S(3); 
+
+% Map volume to a Cartesian grid with indicies for vertex membership
+v  = [];
+vi = [];
+for i = 1:90
+    If = find(ana==i);
+    [xi,yi,zi] = ind2sub(S,If);
+    
+    newv = [x(xi); y(yi); z(zi)]';
+    
+    % assess proximity of verts in this group
+    t = delaunay(newv); t = t(:,1:3);
+    [nf,nv] = reducepatch(maker(t,newv),0.1);
+    newv = nv;
+    
+    newi = ones(length(newv),1)*i;
+    
+    v  = [v;   newv];
+    vi = [vi ; newi];
+end
+
+c = spherefit(v);
+v = v - repmat(c,[size(v,1),1]);
+
+save('~/code/MeshAAL/template/DenseAAL','v','vi')
+
+
