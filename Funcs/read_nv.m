bddd22f25f7ce41488473f240874d218182dfb63
Alex <alexandershaw4@gmail.com>
2019-09-19 12:00:43 +0100

    +

--- Funcs/read_nv.m
+++ Funcs/read_nv.m
@@ -1,7 +1,7 @@
 function mesh = read_nv(X)
 
 if nargin < 1
-    fprintf('Using template\n');
+    %fprintf('Using template\n');
     X = dlmread('BrainMesh_ICBM152_smoothed.nv');
 else
     if ~strcmp(X(end-1:end),'nv')
@@ -11,10 +11,10 @@ else
 end
 
 NVert = X(1);
-fprintf('There are %d vertices ',NVert);
+%fprintf('There are %d vertices ',NVert);
 v     = X(2:NVert+1,:);
 NFace = X(NVert+2);
-fprintf('and %d faces\n',NFace);
+%fprintf('and %d faces\n',NFace);
 f     = X(NVert+3:NVert+3+(NFace-1),:);
 
 mesh.vertices = v;
